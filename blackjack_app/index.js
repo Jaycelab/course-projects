515847507d24f9e1f50e6fb3aec2d1512962b291
Jason Alvarado <jayxalvarado@gmail.com>
2023-08-08 14:34:54 -0700

    updated html, css, and js

--- blackjack_app/index.js
+++ blackjack_app/index.js
@@ -1,18 +1,36 @@
-let firstCard = 11;
+let firstCard = 2;
 let secondCard = 11;
+let cards = [firstCard, secondCard];``
 let sum = firstCard + secondCard;
 let hasBlackJack = false;
 let isAlive = true;
 let message = "";
+let messageEl = document.querySelector("#message-el");
+let sumEl = document.querySelector("#sum-el");
+let cardEl = document.querySelector("#card-el");
 
-if (sum <= 20) {
-  message = "Do you want to hit? 🙂";
-} else if (sum === 21) {
-  message = "Blackjack!!!🥳";
-  hasBlackJack = true;
-} else {
-  message = "Bust!! 😭";
-  isAlive = false;
+function startGame() {
+  renderGame();
 }
 
-console.log(message);
+function renderGame() {
+  sumEl.textContent = "Sum: " + sum;
+  cardEl.textContent = "Cards: " + cards[0] + "-" + cards[1];
+  if (sum <= 20) {
+    message = "Do you want to hit?";
+  } else if (sum === 21) {
+    message = "Blackjack!";
+    hasBlackJack = true;
+  } else {
+    message = "Bust!";
+    isAlive = false;
+  }
+  messageEl.textContent = message;
+}
+
+function newCard() {
+  alert("Drawing a new card from the deck!");
+  let card = 4;
+  sum += card;
+  startGame();
+}

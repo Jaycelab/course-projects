de074608d5169a34a2af3ec1e3f47ed866b5bbc9
jaycelab <jayxalvarado@gmail.com>
2023-08-14 13:26:58 -0700

    finalized blackjack.app

--- blackjack_app/index.js
+++ blackjack_app/index.js
@@ -9,18 +9,38 @@ let messageEl = document.querySelector("#message-el");
 let sumEl = document.querySelector("#sum-el");
 let cardEl = document.querySelector("#card-el");
 
+let player = {
+  name: "Jason",
+  chips: 1000,
+};
+
+let playerEl = document.getElementById("player-el");
+playerEl.textContent = player.name + ": $" + player.chips;
+
 function startGame() {
+  isAlive = true;
+  let firstCard = getRandomCard();
+  let secondCard = getRandomCard();
+  cards = [firstCard, secondCard];
+  sum = firstCard + secondCard;
   renderGame();
 }
 
 function getRandomCard() {
-  return 5;
+  let randomNumber = Math.floor(Math.random() * 13) + 1;
+  if (randomNumber > 10) {
+    return 10;
+  } else if (randomNumber === 1) {
+    return 11;
+  } else {
+    return randomNumber;
+  }
 }
 
 function renderGame() {
   cardEl.textContent = "Cards: ";
   for (let i = 0; i < cards.length; i++) {
-    cardEl.textContent += cards[i] + " ";
+    cardEl.textContent += cards[i] + "  | ";
   }
 
   sumEl.textContent = "Sum: " + sum;
@@ -37,10 +57,10 @@ function renderGame() {
 }
 
 function newCard() {
-  let card = getRandomCard();
-  alert("Drawing a new card from the deck!");
-  sum += card;
-  cards.push(card);
-  console.log(cards);
-  renderGame();
+  if (isAlive === true && hasBlackJack === false) {
+    let card = getRandomCard();
+    sum += card;
+    cards.push(card);
+    renderGame();
+  }
 }

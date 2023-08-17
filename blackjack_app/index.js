624727a4d9de9725de8399acd7a1517365938458
Jason Alvarado <jayxalvarado@gmail.com>
2023-08-16 20:15:13 -0700

    updated blackjack and portfolio folder

--- blackjack_app/index.js
+++ blackjack_app/index.js
@@ -1,20 +1,32 @@
-let firstCard = getRandomCard();
-let secondCard = getRandomCard();
-let cards = [firstCard, secondCard];
-let sum = firstCard + secondCard;
+let cards = [];
+let sum = 0;
 let hasBlackJack = false;
-let isAlive = true;
+let isAlive = false;
 let message = "";
 let messageEl = document.querySelector("#message-el");
 let sumEl = document.querySelector("#sum-el");
 let cardEl = document.querySelector("#card-el");
 
-function startGame() {
-  renderGame();
+function getRandomCard() {
+  let randomNumber = Math.floor(Math.random() * 13) + 1;
+  if (randomNumber > 10) {
+    return 10;
+  } else if (randomNumber === 1) {
+    return 11;
+  } else {
+    return randomNumber;
+  }
 }
 
-function getRandomCard() {
-  return 5;
+function startGame() {
+  isAlive = true;
+  let firstCard = getRandomCard();
+  let secondCard = getRandomCard();
+  cards = [firstCard, secondCard];
+  sum = firstCard + secondCard;
+  // Generate two random numbers
+  // Re-assign the cards and sum variables so that the game can start
+  renderGame();
 }
 
 function renderGame() {
@@ -38,7 +50,7 @@ function renderGame() {
 
 function newCard() {
   let card = getRandomCard();
-  alert("Drawing a new card from the deck!");
+  //alert("Drawing a new card from the deck!");
   sum += card;
   cards.push(card);
   console.log(cards);
